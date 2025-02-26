4a3f4150e1d7f926ed5f5e7c21f179f36ee04ecb
Jakub Marciniak <jakub.marciniak2000@gmail.com>
2025-02-26 01:11:26 +0100

    Initialize Django and Vue project structure with basic configurations and components

---
+++ frontend/src/pages/Home.vue
@@ -0,0 +1,21 @@
+<template></template>
+<script setup>
+import { onMounted } from 'vue'
+import { useAuthStore} from '../store/auth'
+import { useRouter } from 'vue-router
+
+const authStore = useAuthStore()
+const router = useRouter()
+
+async function logout() {
+    try {
+        await authStore.logout()
+    } catch(error) {
+        console.error(error)
+    }
+}
+
+async onMounted(() => {
+    await authStore.fetchUser()
+})
+</script>
---
+++ notshServer/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NotshserverConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'notshServer'
